90c473e2476b8e7ce22e7af96eaee55169ca0ddd
Francerz <francerzam@hotmail.com>
2022-10-31 14:50:01 -0600

    CHG simplifying view structure

--- tests-assets/view.php
+++ tests-assets/view.php
@@ -1,10 +1,6 @@
-<?php
-/** @var \Francerz\WebappRenderUtils\View */
-$view = $view;
-$layout = $view->loadLayout('layout');
-?>
+<?=$layout = $view->loadLayout('layout')?>
 <!-- This comment is ignored because out of layout section -->
-<?php $layout->startSection('content'); ?>
+<?=$layout->startSection('content')?>
 <h1><?=$title?></h1>
 <p><?=$content?></p>
-<?php $layout->endSection(); ?>
+<?=$layout->endSection()?>
